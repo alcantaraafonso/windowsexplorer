8b01879754267fce272276e09f3999929ecafa03
Afonso Alcantara <alcantara.afonso@gmail.com>
2023-04-23 22:20:52 -0300

    Adding domain classes

---
+++ src/main/java/br/com/beganinha/windowsexplorer/core/domain/Drive.java
@@ -0,0 +1,32 @@
+package br.com.beganinha.windowsexplorer.core.domain;
+
+public class Drive {
+
+    private String letter;
+    private long totalCapicty;
+    private long freeCapicty;
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public void setLetter(String letter) {
+        this.letter = letter;
+    }
+
+    public long getTotalCapicty() {
+        return totalCapicty;
+    }
+
+    public void setTotalCapicty(long totalCapicty) {
+        this.totalCapicty = totalCapicty;
+    }
+
+    public long getFreeCapicty() {
+        return freeCapicty;
+    }
+
+    public void setFreeCapicty(long freeCapicty) {
+        this.freeCapicty = freeCapicty;
+    }
+}
